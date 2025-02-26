89167a55eab3518291a4f006e54e3c4d8cd5cfa9
Diego Trejo <diegoinn96@gmail.com>
2025-02-25 19:00:08 -0600

    added neovim config

---
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,17 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'terrortylor/nvim-comment',
+    config = function()
+      require('nvim_comment').setup { create_mappings = false }
+    end,
+  },
+  {
+    'Aasim-A/scrollEOF.nvim',
+    event = { 'CursorMoved', 'WinScrolled' },
+    opts = {},
+  },
+}
