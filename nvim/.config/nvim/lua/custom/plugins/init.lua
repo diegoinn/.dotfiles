cbc1392ee0e2611ddafed61ec903d3c0d1606d40
Diego Trejo <diegoinn96@gmail.com>
2025-02-26 08:17:11 -0600

    Added more configs

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -2,16 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  {
-    'terrortylor/nvim-comment',
-    config = function()
-      require('nvim_comment').setup { create_mappings = false }
-    end,
-  },
-  {
-    'Aasim-A/scrollEOF.nvim',
-    event = { 'CursorMoved', 'WinScrolled' },
-    opts = {},
-  },
-}
+return {}
